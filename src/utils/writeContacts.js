de4a8975633770b3de1e580e1f6b6c55de6eda4b
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-10-25 10:40:41 +0300

    25/10/24

--- src/utils/writeContacts.js
+++
@@ -1,5 +0,0 @@
-import { PATH_DB } from '../constants/contacts.js';
-import { writeFile } from 'node:fs/promises';
-
-export const writeContacts = (contacts) =>
-  writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
