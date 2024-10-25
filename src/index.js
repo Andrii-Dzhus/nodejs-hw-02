8cda457072bc6147265f0648354938e44678e136
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-10-25 11:48:36 +0300

    11:50

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { startServer } from './server';
+import { startServer } from './server.js';
 
 const boostrap = async () => {
   startServer();
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import { env } from './utils/env.js';
 
 export const startServer = () => {
   const app = express();
@@ -27,5 +28,7 @@ export const startServer = () => {
       message: error.message,
     });
   });
-  app.listen(3000, () => console.log('Server running on 3000 PORT'));
+
+  const port = Number(env('PORT', 3000));
+  app.listen(port, () => console.log(`Server running on ${port} PORT`));
 };
