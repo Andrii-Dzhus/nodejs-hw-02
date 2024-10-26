3d06802219c4e8f6efa0ce6cf3ecb482584c3af8
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-10-26 13:02:23 +0300

    13:00

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
@@ -14,6 +14,12 @@ export const setupServer = () => {
     },
   });
 
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Start project',
+    });
+  });
+
   app.get('/contacts', async (req, res) => {
     const data = await contactServices.getContacts();
     res.json({
@@ -23,11 +29,23 @@ export const setupServer = () => {
     });
   });
 
-  // app.get('/contacts', (req, res) => {
-  //   res.json({
-  //     message: 'Start project',
-  //   });
-  // });
+  app.get('/contacts/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await contactServices.getContactById(id);
+
+    if (!data) {
+      return res.status(404).json({
+        status: 404,
+        message: `Contact not found`,
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully found contact with id ${id}!`,
+      data,
+    });
+  });
 
   app.use((req, res) => {
     res.status(404).json({
