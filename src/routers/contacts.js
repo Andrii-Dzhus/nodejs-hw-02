13c6beff450b02bec05bd3980ae653d4b19b3d51
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-11-08 15:46:05 +0200

    08/11/24

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,24 +1,39 @@
 import { Router } from 'express';
 import * as contactsControllers from '../controllers/contacts.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
+import validateBody from '../utils/validateBody.js';
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+} from '../validation/contacts.js';
+import { isValueId } from '../middlewares/isValidId.js';
+
 const contactsRouret = Router();
 
 contactsRouret.get('/', ctrlWrapper(contactsControllers.getContactsController));
 
 contactsRouret.get(
   '/:id',
+  isValueId,
   ctrlWrapper(contactsControllers.getContactsByIdController),
 );
 
-contactsRouret.post('/', ctrlWrapper(contactsControllers.addContactController));
+contactsRouret.post(
+  '/',
+  validateBody(contactAddSchema),
+  ctrlWrapper(contactsControllers.addContactController),
+);
 
 contactsRouret.patch(
   '/:id',
+  isValueId,
+  validateBody(contactUpdateSchema),
   ctrlWrapper(contactsControllers.patchContactController),
 );
 
 contactsRouret.delete(
   '/:id',
+  isValueId,
   ctrlWrapper(contactsControllers.deleteContactController),
 );
 export default contactsRouret;
