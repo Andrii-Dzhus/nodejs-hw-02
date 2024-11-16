39edaa884bf03805955a4a56f447f13c930246b3
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-11-16 21:33:22 +0200

    21:30

--- src/services/auth.js
+++ src/services/auth.js
@@ -71,6 +71,9 @@ export const refreshUserSession = async ({ sessionId, refreshToken }) => {
   });
 };
 
+export const logout = (sessionId) =>
+  SessionCollection.deleteOne({ _id: sessionId });
+
 export const findSession = (filter) => SessionCollection.findOne(filter);
 
 export const findUser = (filter) => UserCollection.findOne(filter);
